def92fab4ba0f091e14166805c28e324e5bca782
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-11 22:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+const destructivelyAppendDriver=n=>drivers.push(n);
+const destructivelyPrependDriver=n=>drivers.unshift(n);
+const destructivelyRemoveLastDriver=_=>drivers.pop();
+const destructivelyRemoveFirstDriver=_=>drivers.shift();
+const appendDriver
